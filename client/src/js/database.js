fb4022e310796fdce87e24322b7dd60958ac228f
spmoroney <spmoroney@att.net>
2023-11-12 12:51:34 -0500

    Resolve remaining todo's by addinng data functions and install events.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+  export const putDb = async (content)  => {
+    console.log('PUT to the database');
+  
+    //Create a connection to the chosen database with the chosen database version.
+    const contactDb = await openDB('jate', 1);
+  
+    //Make a transation with the database and the privileges we need.
+    const tx = contactDb.transaction('jate', 'readwrite');
+  
+    //Open up an object store for the transaction... seems like too many steps to me.
+    const store = tx.objectStore('jate');
+  
+    //invoke the put method on the created store and store the procided content  
+    const request = store.put({ id: 1, value: content });
+  
+    //Get confirmation of the result and log it.
+    const result = await request;
+    console.log('Data saved to the database', result);
+  };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  export const getDb = async () => {
+    console.log('GET from the database');
+  
+    //Open a connection to the chosen database with the chosen version.
+    const contactDb = await openDB('jate', 1);
+  
+    //Make a new transaction with the chosen database and privilages.
+    const tx = contactDb.transaction('jate', 'readonly');
+  
+    //Open up that object store...
+    const store = tx.objectStore('jate');
+  
+    //Invoke getAll() to get the data out.
+    const request = store.getAll();
+  
+    //Get confirmation and then log it. 
+    const result = await request;
+    console.log('The result value:', result);
+    return result?.value;
+  };
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    //Save the triggered event in the window's deferred prompt.
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    //Make the button show on the window by taking off the hidden class. 
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt; //retrieve the deferred event. 
+    if (!promptEvent) { //if there isn't one, we have to abort.
+        return;
+    }
+
+    // Show the retrieved prompt
+    promptEvent.prompt();
+
+    //We only want the prompt to be used once. 
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true); //hide the button when we're done with it.
+});
+
+window.addEventListener('appinstalled', (event) => {
+    //After we're done installing we can clear the prompt.  
+    window.deferredPrompt = null;
+}); 
